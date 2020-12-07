e18e5455fcf64b61a4690ab99596c52014d19b53
Anton Tsvetkov <anton_tsvetkov@epam.com>
2020-12-07 15:30:24 +0300

    add aircraft carrier structure

---
+++ Interfaces_Annotations_Task_8/src/AircraftCarrier.java
@@ -0,0 +1,152 @@
+import java.util.Arrays;
+
+public class AircraftCarrier extends WarShip{
+
+
+    Aircraft[] aircrafts;
+
+
+
+    public AircraftCarrier(String name, String serialNumb,
+                           int displacement, int length, int width, int height,
+                           Engine engine) {
+        super(name, serialNumb, displacement, length, width, height, engine);
+    }
+
+    public AircraftCarrier(String name, String serialNumb,
+                           int displacement, int length, int width, int height,
+                           String enginePower, String engineVolume) {
+        super(name, serialNumb, displacement, length, width, height, enginePower, engineVolume);
+    }
+
+    public AircraftCarrier(String name, String serialNumb,
+                           int displacement, int length, int width, int height,
+                           Engine engine, Aircraft[] aircrafts) {
+        super(name, serialNumb, displacement, length, width, height, engine);
+        this.aircrafts = aircrafts;
+    }
+
+    public AircraftCarrier(String name, String serialNumb,
+                           int displacement, int length, int width, int height,
+                           String enginePower, String engineVolume,
+                           Aircraft[] aircrafts) {
+        super(name, serialNumb, displacement, length, width, height, enginePower, engineVolume);
+        this.aircrafts = aircrafts;
+    }
+
+    public Aircraft[] getAircrafts() {
+        return aircrafts;
+    }
+
+    public void setAircrafts(Aircraft[] aircrafts) {
+        this.aircrafts = aircrafts;
+    }
+
+    public class Aircraft{
+
+        private String type;
+        private String location;
+
+        private int weight;
+        private int length;
+        private int width;
+        private int height;
+
+        private boolean onBoard;
+
+
+        public Aircraft(String type, int weight, int length, int width, int height, String location, boolean onBoard) {
+            this.type = type;
+            this.weight = weight;
+            this.length = length;
+            this.width = width;
+            this.height = height;
+            this.location = location;
+            this.onBoard = onBoard;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        public boolean isOnBoard() {
+            return onBoard;
+        }
+
+        public void setOnBoard(boolean onBoard) {
+            this.onBoard = onBoard;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public void setLength(int length) {
+            this.length = length;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public void setWidth(int width) {
+            this.width = width;
+        }
+
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+
+        @Override
+        public String toString() {
+            return "Aircraft{" +
+                    "type='" + type + '\'' +
+                    ", weight=" + weight +
+                    ", length=" + length +
+                    ", width=" + width +
+                    ", height=" + height +
+                    ", onBoard=" + onBoard +
+                    '}';
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "AircraftCarrier{" +
+                "aircrafts=" + Arrays.toString(aircrafts) +
+                ", name='" + getName() + '\'' +
+                ", serialNumb='" + getSerialNumb() + '\'' +
+                ", displacement=" + getDisplacement() +
+                ", length=" + getLength() +
+                ", width=" + getWidth() +
+                ", height=" + getHeight() +
+                ", engine=" + getEngine() +
+                ", isSail=" + isSail() +
+                '}';
+    }
+}
